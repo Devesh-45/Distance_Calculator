30138a94edd3601e864eac742b6dfe0d46604565
Devesh-45 <devesh.ashok02@gmail.com>
2024-05-27 20:11:51 +0530

    Task Done

---
+++ main.py
@@ -0,0 +1,7 @@
+print("DISTANCE CALCULATOR")
+x1,y1=list(map(int,input("Enter point 1'sco-ordinates").split(",")))
+x2,y2=list(map(int,input("Enter point 2'sco-ordinates").split(",")))
+Dist=(((x2-x1)**0.5)+((y2-y1)**0.5))**0.5
+print("Do you want to continue or exit?")
+
+
